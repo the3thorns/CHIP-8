e62cecbeca6610433f4715fb4472b849783d1aff
the3thorns <the3thorns@gmail.com>
2024-10-23 16:11:53 +0200

    modified macros and added heades guards to cpu.h

--- src/cpu.h
+++ src/cpu.h
@@ -1,3 +1,6 @@
+#ifndef CPU_H_
+#define CPU_H_
+
 #include "common.h"
 #include "memory.h"
 
@@ -6,9 +9,12 @@
  * Masking macros
  */
 
-#define MASK_FIRST          0b1111000000000000
-#define MASK_LAST_TWO       0b0000000011111111
+#define MASK_FIRST_NIBBLE       0xF000
+#define MASK_SECOND_NIBBLE      0x0F00
+#define MASK_THIRD_NIBBLE       0x00F0
+#define MASK_FOURTH_NIBBLE      0x000F
 
+#define MASK_LEAST_SIG_BIT      0x0001
 /**
  * Here CPU logic and instruction execution will be implemented.
  */
@@ -17,35 +23,31 @@ namespace cp8 {
 
     class Cpu {
         private:
+            /**
+             * Memory class
+             */
             Memory memory;
-            // Registers
-            byte v0;
-            byte v1;
-            byte v2;
-            byte v3;
-            byte v4;
-            byte v5;
-            byte v6;
-            byte v7;
-            byte v8;
-            byte v9;
-            byte va;
-            byte vb;
-            byte vc;
-            byte vd;
-            byte ve;
-            byte vf;
-
-            address pc; // Represents the current instruction about to execute
+            
+            /**
+             * Registers
+             */
+            byte registers[16] = {0};
+            // Program counter
+            address pc;
+            // Register used to store memory addresses
             address i;
 
-            // Private instructions
-
+            /**
+             * Private functions
+             */
             byte mask(instruction ins, uint16_t mask, byte size);
             void execute_instruction(instruction ins);
+            void next_instruction();
 
         public:
             Cpu();
             void run(); // Starts the cpu execution
     };
 }
+
+#endif
