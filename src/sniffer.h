d10b155eecacbb9de0144d148ab08bb4fb335651
the3thorns <the3thorns@gmail.com>
2024-10-24 17:06:32 +0200

    added sniffer macros and modified show_cpu_status

--- src/sniffer.h
+++ src/sniffer.h
@@ -1,10 +1,16 @@
 #ifndef SNIFFER_H_
 #define SNIFFER_H_
 
+#include <iomanip>
+
 #include "common.h"
 #include "cpu.h"
 
-#define register(x) this->status->registers[(x)]
+#define REGISTER(x) this->status->registers[(x)]
+#define SHOW_HEX    std::setbase(16) << std::showbase
+#define CLEAR_HEX   std::resetiosflags(std::ios_base::basefield)
+#define PC          this->status->pc
+#define I           this->status->i
 
 /**
  * This class is atached to the CPU and prompts the state of the interpreter
@@ -19,7 +25,7 @@ namespace cp8 {
         public:
             SnifferCH8(Cpu *cpu);
             void execute_instruction(instruction ins);
-            void show_cpu_status();
+            void show_cpu_status(instruction ins);
     };
 }
 
