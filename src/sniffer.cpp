598fde7170ccc912dcae5977777db71894fdec97
the3thorns <the3thorns@gmail.com>
2024-10-24 17:06:54 +0200

    added show_cpu_status

--- src/sniffer.cpp
+++ src/sniffer.cpp
@@ -10,10 +10,24 @@ namespace cp8 {
         cpu->embedd_instruction(ins);
 
         // Show status of the CPU
-        this->show_cpu_status();
+        this->show_cpu_status(ins);
     }
 
-    void SnifferCH8::show_cpu_status() {
-        // TODO
+    void SnifferCH8::show_cpu_status(instruction ins) {
+        std::cout << std::endl;
+
+        std::cout << "Instruction: ";
+        std::cout << SHOW_HEX << ins << std::endl;
+        std::cout << CLEAR_HEX;
+        std::cout << std::endl;
+
+        for (int i = 0; i < 8; i++) {
+            std::cout << "v" << i << ": " << SHOW_HEX << (int) REGISTER(i) << CLEAR_HEX << std::setw(8) 
+                << "v" << i + 8 << ": " << SHOW_HEX << (int) REGISTER(i + 8) << CLEAR_HEX << std::endl;
+        }
+        std::cout << "pc: " << SHOW_HEX << PC << CLEAR_HEX << std::endl;
+        std::cout << "i:  " << SHOW_HEX << I << CLEAR_HEX << std::endl;
+        
+        std::cout << std::endl;
     }
 }
