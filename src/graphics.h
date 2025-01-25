9b40c2fe736f27690c286a0e67fae7cf878189df
thorns <the3thorns@gmail.com>
2025-01-25 10:24:40 +0100

    Added draw_sprite and updated init_window

--- src/graphics.h
+++ src/graphics.h
@@ -6,19 +6,30 @@
  * The graphics.c file contains the implementation on raylib but it can be changed to any other graphics library
  */
 
+#include "common.h"
+
+#define STANDARD_WIDTH  64
+#define STANDARD_HEIGHT 32
+
 
 /*
  * Creates the window
  */
-void ch8g_init_window();
+void ch8g_init_window(int width, int height);
 
 
-/*
- * Draws a pixel on the screen.
- * If the coordinates are not valid, will do nothing
+/**
+ * Draws a sprite on the coordinates x, y
+ * Width: 8 pixels
+ * Height: N pixels
  */
 
-void ch8g_draw_pixel(int px, int py);
+void ch8g_draw_sprite(int x, int y, int N, byte* memory, address i, byte* vf);
+
+/*
+ * Clears the screen
+ */
+void ch8g_clear_screen();
 
 
 #endif // GRAPHICS_H_
